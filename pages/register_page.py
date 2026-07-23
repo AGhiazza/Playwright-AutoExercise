b812ad5c03a88d03d4ca6725e8e0df4f69042ebc
Adrian Ghiazza <afg.hack@hotmail.com>
2026-07-23 17:44:42 -0300

    feat: add RegisterPage with account and address selectors

---
+++ pages/register_page.py
@@ -0,0 +1,90 @@
+from pages.base_page import BasePage
+
+class RegisterPage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+
+    #Selectors
+
+    ##Register Selectors
+
+    @property
+    def create_account_button(self):
+        return self.page.locator("[data-qa='create-account']")
+
+    ###Account Information Selectors
+    @property
+    def acc_title_mr(self):
+        return self.page.locator("#id_gender1")
+    
+    @property
+    def acc_title_mrs(self):
+        return self.page.locator("#id_gender2")
+    
+    @property
+    def acc_name(self):
+        return self.page.locator("[data-qa='name']")
+
+    @property
+    def acc_email(self):
+        return self.page.locator("[data-qa='email']")
+    
+    @property
+    def acc_password(self):
+        return self.page.locator("[data-qa='password']")
+
+    ###Address Information Selectors
+
+    @property
+    def address_info_name(self):
+        return self.page.locator("[data-qa='first_name']")
+    
+    @property
+    def address_info_lastname(self):
+        return self.page.locator("[data-qa='last_name']")
+    
+    @property
+    def address_info_address(self):
+        return self.page.locator("[data-qa='address']")
+    
+    @property
+    def address_info_country(self):
+        return self.page.locator("[data-qa='country']")
+    
+    @property
+    def address_info_state(self):
+        return self.page.locator("[data-qa='state']")
+    
+    @property
+    def address_info_city(self):
+        return self.page.locator("[data-qa='city']")
+    
+    @property
+    def address_info_zipcode(self):
+        return self.page.locator("[data-qa='zipcode']")
+    
+    @property
+    def address_info_mobile_number(self):
+        return self.page.locator("[data-qa='mobile_number']")
+    
+    ###Account Created Selectors
+
+    @property
+    def account_created_message(self):
+        return self.page.locator("[data-qa='account-created']")
+
+    @property
+    def account_created_continue_button(self):
+        return self.page.locator("[data-qa='continue-button']")
+
+
+    ##Account Information Methods
+
+    def fill_account_info(self):
+
+    ##Address Information Methods
+
+    def fill_address_info(self): 
+
+    def click_on_create_account(self):
+        self.create_account_button.click()
