65042e8cbd6996c5e8f3c105464fc265d385dea4
Adrian Ghiazza <afg.hack@hotmail.com>
2026-07-31 16:24:09 -0300

    test: add UI_PD01 write product review

--- pages/product_detail_page.py
+++ pages/product_detail_page.py
@@ -12,3 +12,32 @@ class ProductDetailPage(BasePage):
     def product_name(self):
         return self.page.locator(".product-information h2")
     
+    ##Review Selectors
+
+    @property
+    def review_name(self):
+        return self.page.locator("#name")
+    
+    @property
+    def review_email(self):
+        return self.page.locator("#email")
+    
+    @property
+    def review_textbox(self):
+        return self.page.locator("#review")
+    
+    @property
+    def submit_review_button(self):
+        return self.page.locator("#button-review")
+    
+    @property
+    def successful_review_submission_message(self):
+        return self.page.locator("#review-section .alert-success")
+
+    ##Review Methods
+
+    def write_a_review(self, name, email, review):
+        self.review_name.fill(name)
+        self.review_email.fill(email)
+        self.review_textbox.fill(review)
+        self.submit_review_button.click()
---
+++ tests_ui/test_product_detail.py
@@ -0,0 +1,14 @@
+from pages.products_page import ProductsPage
+from pages.product_detail_page import ProductDetailPage
+from utils.data_reader import read_json
+
+test_data = read_json("user_data.json")
+review_data = test_data["review"]
+
+def test_PD01_write_a_review(page):
+    products_page = ProductsPage (page)
+    product_detail_page = ProductDetailPage(page)
+    page.goto("/products")
+    products_page.click_on_first_view_product_details()
+    product_detail_page.write_a_review(review_data["name"], review_data["email"], review_data["review"])
+    assert product_detail_page.successful_review_submission_message.is_visible()
