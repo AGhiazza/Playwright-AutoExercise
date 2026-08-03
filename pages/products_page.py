b3bdd6e21d07138f92ac2aff48d6e0bf62de07d5
Adrian Ghiazza <afg.hack@hotmail.com>
2026-08-03 15:28:19 -0300

    test: add UI_PR01 and UI_PR02 product search tests

--- pages/products_page.py
+++ pages/products_page.py
@@ -8,15 +8,36 @@ class ProductsPage(BasePage):
 
     ##Navigation Selectors
     @property
-    def category_breadcrumb (self):
+    def category_breadcrumb(self):
         return self.page.locator(".breadcrumb .active")
 
     @property
     def first_view_product_link(self):
         return self.page.locator("a[href*='/product_details/']").first
-    
+
+    ##Search Selectors
+
+    @property
+    def search_bar(self):
+        return self.page.locator("#search_product")
+
+    @property
+    def search_button(self):
+        return self.page.locator("#submit_search")
+
+    @property
+    def products_section_title(self):
+        return self.page.locator(".title.text-center")
+
+    @property
+    def product_cards(self):
+        return self.page.locator(".productinfo")
 
     # Navigation Methods
 
     def click_on_first_view_product_details(self):
         self.first_view_product_link.click()
+
+    def search_for_product(self, product):
+        self.search_bar.fill(product)
+        self.search_button.click()
---
+++ tests_ui/test_products.py
@@ -0,0 +1,19 @@
+from pages.products_page import ProductsPage
+from utils.data_reader import read_json
+
+test_data = read_json("navigation_data.json")
+valid_search_term = test_data["valid_search_term"]
+invalid_search_term = test_data["invalid_search_term"]
+
+def test_PR01_search_for_product(page):
+    products_page = ProductsPage(page)
+    page.goto("/products")
+    products_page.search_for_product(valid_search_term)
+    assert "SEARCHED PRODUCTS" in products_page.products_section_title.inner_text()
+    assert products_page.product_cards.count() > 0
+
+def test_PR02_search_for_nonexisting_product(page):
+    products_page = ProductsPage(page)
+    page.goto("/products")
+    products_page.search_for_product(invalid_search_term)
+    assert products_page.product_cards.count() == 0
