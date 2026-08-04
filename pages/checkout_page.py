34d399fee4ad46693d655a60644c780797b53ba9
Adrian Ghiazza <afg.hack@hotmail.com>
2026-08-04 15:31:55 -0300

    test: add UI_CH02 verify address details in checkout

---
+++ pages/checkout_page.py
@@ -0,0 +1,23 @@
+from pages.base_page import BasePage
+
+class CheckoutPage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+
+    #Selectors
+
+    ##Address Details Selectors
+
+    @property
+    def delivery_firstname_lastname(self):
+        return self.page.locator("#address_delivery .address_firstname.address_lastname")
+
+    @property
+    def delivery_address(self):
+        return self.page.locator("#address_delivery .address_address1.address_address2:not(:empty)")
+
+    @property
+    def delivery_country(self):
+        return self.page.locator("#address_delivery .address_country_name")
+    
+    ##Review Your Order Selectors
--- tests_ui/test_checkout.py
+++ tests_ui/test_checkout.py
@@ -1,7 +1,8 @@
 import pytest
+from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 from pages.cart_page import CartPage
-from pages.login_page import LoginPage
+from pages.checkout_page import CheckoutPage
 from utils.data_reader import read_json
 
 test_data = read_json("user_data.json")
@@ -16,5 +17,16 @@ def test_CH01_attempt_checkout_with_no_signin(page):
     cart_page.checkout_button.click()
     assert cart_page.checkout_login_modal.is_visible()
 
-
-    
+def test_CH02_verify_address_details_in_checkout(page, registered_user):
+    registered_address = user_data["address"]
+    login_page = LoginPage(page)
+    products_page = ProductsPage(page)
+    cart_page = CartPage(page)
+    checkout_page = CheckoutPage(page)
+    page.goto("/login")
+    login_page.login(user_data["email"], user_data["password"])
+    products_page.first_product_add_to_cart_button.click()
+    products_page.view_cart_button.click()
+    cart_page.checkout_button.click()
+    checkout_address = checkout_page.delivery_address.inner_text()
+    assert checkout_address == registered_address
