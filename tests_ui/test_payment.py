be46570dc0f5ecc409f11428b4ac15ba90fc15b6
Adrian Ghiazza <afg.hack@hotmail.com>
2026-08-05 15:26:58 -0300

    test: add UI_PY02 download invoice after order

--- tests_ui/test_payment.py
+++ tests_ui/test_payment.py
@@ -21,3 +21,28 @@ def test_PY01_complete_an_order(page, registered_user):
     checkout_page.place_order_button.click()
     payment_page.fill_payment_details(payment_data["card_name"], payment_data["card_number"], payment_data["card_cvc"], payment_data["card_month"], payment_data["card_year"])
     assert payment_page.order_placed_message.is_visible()
+
+def test_PY02_download_invoice_after_successful_order(page, registered_user):
+    login_page = LoginPage(page)        
+    cart_page = CartPage(page)
+    checkout_page = CheckoutPage(page)
+    payment_page = PaymentPage(page)
+    page.goto("/login")
+    login_page.login(user_data["email"], user_data["password"])
+    cart_page.first_product_add_to_cart_button.click()
+    cart_page.view_cart_button.click()
+    first_product_price = cart_page.first_product_price.inner_text() #Saves product price
+    price_number = first_product_price.replace("Rs. ", "") #Cleans currency symbol
+    cart_page.checkout_button.click()
+    checkout_page.place_order_button.click()
+    payment_page.fill_payment_details(payment_data["card_name"], payment_data["card_number"], payment_data["card_cvc"], payment_data["card_month"], payment_data["card_year"])
+    with page.expect_download() as download_info:
+        payment_page.download_invoice_button.click()
+    download = download_info.value
+    download_path = download.path()
+    with open(download_path, "r") as f:
+        content = f.read()
+    assert price_number in content 
+    assert user_data["firstname"] in content
+    assert "Thank you" in content
+  
